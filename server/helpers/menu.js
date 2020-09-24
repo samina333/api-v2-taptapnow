d5c9ec372eb9afb8ac956ddadc1b4e92c8d28b51
Samina Munsib <saminakhan54688@gmail.com>
2020-09-24 23:50:12 +0500

    up

--- server/helpers/menu.js
+++ server/helpers/menu.js
@@ -1,7 +1,8 @@
 const Menu = require('../../database/helpers/menu.js');
 const fs = require('fs');
+const db = require('../../database/config.js');
 
-exports.saveMenu = (req, res) => {
+exports.saveMenu = (req, res , cb) => {
     let data = req.body;
     data.prix_menu = parseFloat(data.prix_menu);
     if(req.file){
@@ -18,7 +19,12 @@ exports.saveMenu = (req, res) => {
             data.img_menu = target_path.substring(14)
             Menu.saveMenu(data, (result) => {
               if(result){
-                res.status(200).send(result);
+                //res.status(200).send(result);
+                query = `update restaurant set takeout= ${1} WHERE id_restaurant = '${req.body.restaurantMenuID}'`;
+          
+          db.knex.raw(query).then(function (response) {
+            cb(response[0]);
+            }).catch(error => cb(error))
               }
               else {
                 res.status(400).send({result: "can't save new menu"});
@@ -30,14 +36,19 @@ exports.saveMenu = (req, res) => {
       data.img_menu = "https://res.cloudinary.com/transition-digital/image/upload/v1579733572/New_Project_2_fymcr2.png";
       Menu.saveMenu(data, (result) => {
         if(result){
-          res.status(200).send(result);
+          //res.status(200).send(result);
+          query = `update restaurant set takeout= ${1} WHERE id_restaurant = '${req.body.restaurantMenuID}'`;
+          
+          db.knex.raw(query).then(function (response) {
+            cb(response[0]);
+            }).catch(error => cb(error))
         }
         else {
           res.status(400).send({result: "can't save new menu"});
         }
       })
     }
-  }
+  } 
 
 exports.deleteMenu = (req, res) => {
   let menu_id = req.params.menu_id;
